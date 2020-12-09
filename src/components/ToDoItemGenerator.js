a7c75c36689801b6d13742bf5d55a7eb28a1694b
Hins Chiu <chiuhi2@oocl.com>
2020-12-09 09:48:30 +0800

    [toDoList][HinsChiu]refactor:refactor with antd Form

--- src/components/ToDoItemGenerator.js
+++ src/components/ToDoItemGenerator.js
@@ -1,41 +1,29 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { Form, Input, Button } from 'antd';
+import FormItem from 'antd/lib/form/FormItem';
 
 class toDoItemGenerator extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            input : ""
-        };
-    }
-
-    onInputValueChange = (event) => {
-        this.setState({
-            input : event.target.value
-        });
-    }
-
-    addToDo = () => {
-        const { input } = this.state;
-
-        if(input.length > 0) {
-            var toDo = {
-                id : uuidv4(),
-                message : input,
-                done : false
-            }
-
-            this.props.addToDo(toDo);
+    addToDo = (values) => {
+        var toDo = {
+            id : uuidv4(),
+            message : values.message,
+            done : false
         }
+
+        this.props.addToDo(toDo);
     }
 
     render() {
         return (
-            <div>
-                <input type="text" placeholder="Input a new todo here." onChange={this.onInputValueChange}/>
-                <button onClick={this.addToDo}>add</button>
-            </div>
+            <Form onFinish={(values) => this.addToDo(values)}>
+                <FormItem name="message" rules={[{required: true, message : "Input is required."}]}>
+                    <Input placeholder="Input a new todo here." />
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit">add</Button>
+                </FormItem>
+            </Form>
         );
     }
 }

e051db9841e1b971a3072aced36de5e5955e2bd8
Hins Chiu <chiuhi2@oocl.com>
2020-12-09 08:53:40 +0800

    [toDoList][HinsChiu]reafactor:refactor

--- src/containers/ToDoGroupContainer.js
+++ src/containers/ToDoGroupContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import ToDoGroup from '../components/ToDoGroup';
 
 const mapStateToProps = (state) => ({
-    toDoList : state.toDoList
+    toDoList : state
 })
 
 const ToDoGroupContainer = connect(mapStateToProps)(ToDoGroup);
--- src/reducers.js
+++ src/reducers.js
@@ -1,26 +1,16 @@
 import { ADD_TO_DO, REMOVE_TO_DO, UPDATE_DONE_STATUS } from './actionTypes';
 
-const initState = {
-    toDoList : []
-}
-
-const reducer = (state = initState, action) => {
+const toDoList = (state = [], action) => {
     switch(action.type) {
         case ADD_TO_DO :
-            return {
-                toDoList : state.toDoList.concat(action.payload)
-            };
+            return state.concat(action.payload);
         case REMOVE_TO_DO : 
-            return {
-                toDoList : state.toDoList.filter((item) => item.id !== action.payload)
-            };
+            return state.filter((item) => item.id !== action.payload);
         case UPDATE_DONE_STATUS : 
-            return {
-                toDoList : state.toDoList.map((item) => item.id === action.payload ? {...item, done : item.done ? false : true} : item)
-            };
+            return state.map((item) => item.id === action.payload ? {...item, done : item.done ? false : true} : item);
         default : 
             return state;
     }
 }
 
-export default reducer;
+export default toDoList;

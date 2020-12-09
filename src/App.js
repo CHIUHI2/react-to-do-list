af7629b445c5732940f540742e5ab1b69c126103
Hins Chiu <chiuhi2@oocl.com>
2020-12-09 16:13:34 +0800

    [toDoList][HinsChiu]refactor:refactor with Menu

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import ToDoList from './components/ToDoList'
 import DoneGroupContainer from './containers/DoneGroupContainer';
 import NotFound from "./components/NotFound"
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { Menu } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-          <ul>
-            <li>
+          <Menu mode="horizontal">
+            <Menu.Item key="home">
               <Link to="/">Home</Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item key="done-list">
               <Link to="/done-list">Done List</Link>
-            </li>
-          </ul>
+            </Menu.Item>
+          </Menu>
           <Switch>
             <Route exact path="/done-list" component={DoneGroupContainer} />
             <Route exact path="/" component={ToDoList} />
